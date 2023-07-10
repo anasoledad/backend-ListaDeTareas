173c0762f0cdd5f41952be02681e307d6f76684f
Ana Rocha <anasoledad.rocha@gmail.com>
2023-07-10 02:51:38 -0300

    Se agregó la ruta y el controlador para poder crear una tarea

--- src/controllers/tareas.controllers.js
+++ src/controllers/tareas.controllers.js
@@ -1,4 +1,5 @@
 import Tarea from "../models/tarea";
+import { validationResult } from "express-validator";
 
 export const obtenerTareas = async(req, res)=>{
     try{
@@ -11,3 +12,21 @@ export const obtenerTareas = async(req, res)=>{
         })
     }
   }
+
+  export const crearTarea = async(req, res)=>{
+    try{
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({errores: errors.array()})
+        }
+        const tareaNueva = new Tarea(req.body);
+        await tareaNueva.save();
+        res.status(201).json({
+            mensaje: 'La tarea fue creada correctamente'
+        });
+    }catch (error){
+        res.status(404).json({
+            mensaje: 'Error al crear la tarea'
+        })
+    }
+}
--- src/routers/tareas.routes.js
+++ src/routers/tareas.routes.js
@@ -2,21 +2,20 @@
 //     res.send('esta es una prueba de la peticion');
 // })
 import { Router } from "express";
-import { obtenerTareas } from "../controllers/tareas.controllers";
+import { crearTarea, obtenerTareas } from "../controllers/tareas.controllers";
 
 const router = Router();
 
 router
   .route("/tareas").get(obtenerTareas)
-//   .get(obtenerProductos)
-//   .post(
+  .post(
 //     [
 //       check("nombreProducto")
 //         .notEmpty()
 //         .withMessage("El nombre del producto es obligatorio"),
 //     ],
-//     crearProducto
-//   );
+    crearTarea
+  );
 // router
 //   .route("/productos/:id")
 //   .delete(borrarProducto)
