885a3d8c0dc8e10ff969cffbfe46eb11698939cc
Ana Rocha <anasoledad.rocha@gmail.com>
2023-07-10 00:01:55 -0300

    Se agregó los middlewares

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from 'express';
-
+import cors from 'cors'
+import morgan from 'morgan';
+import * as dotenv from 'dotenv';
 
 
 
@@ -9,4 +11,14 @@ const app = express();
 app.set("port", process.env.PORT || 4000);
 app.listen(app.get("port"), ()=>{
     console.log('Estoy en el puerto '+ app.get("port"));
+})
+
+
+app.use(cors());
+app.use(express.json()); 
+app.use(express.urlencoded({extended: true}));
+app.use(morgan('dev')); 
+
+app.get('/prueba', (req, res)=>{
+    res.send('esta es una prueba de la peticion');
 })
