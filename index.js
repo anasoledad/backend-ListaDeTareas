d20bb48e8c92c1f0d080ba784933d0d730be7dc1
Ana Rocha <anasoledad.rocha@gmail.com>
2023-07-10 00:00:31 -0300

    Se agregó un puerto

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 import express from 'express';
-console.log('es una prueba')
+
+
+
+
+
+const app = express();
+
+app.set("port", process.env.PORT || 4000);
+app.listen(app.get("port"), ()=>{
+    console.log('Estoy en el puerto '+ app.get("port"));
+})
